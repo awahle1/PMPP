ea4a7d3fc6a37ce54093322383278ce37f8aac9b
Austin Wahle <austinwahle1@gmail.com>
2025-05-13 03:42:51 +0000

    finished vec add

--- first-kernel/vector_add.cu
+++ first-kernel/vector_add.cu
@@ -1,4 +1,17 @@
-void vecAdd(float* A, float* B, float* C, int n) {
+#include <iostream>
+#include <cuda_runtime.h>
+#include <iomanip>
+
+
+__global__
+void vecAddKernel(float* A, float* B, float* C, int n) {
+int i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < n) {
+        C[i] = A[i] + B[i];
+    }
+}
+
+void vecAdd(float* A_h, float* B_h, float* C_h, int n) {
     int size = n* sizeof(float);
 
     float *A_d;
@@ -6,14 +19,14 @@ void vecAdd(float* A, float* B, float* C, int n) {
     float *B_d;
     cudaMalloc((void**)&B_d, size);
     float *C_d;
-    cudaMalloc((void**)^C_d, size);
+    cudaMalloc((void**)&C_d, size);
 
     cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(B_d, A_h, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
 
-    
+    vecAddKernel<<<ceil(n/256.0), 256>>>(A_d, B_d, C_d, n);
 
-    cudaMemcpy(C_h, C_d, size, cudeMemcpyDeviceToHost);
+    cudaMemcpy(C_h, C_d, size, cudaMemcpyDeviceToHost);
 
     cudaFree(C_d);
     cudaFree(B_d);
@@ -22,5 +35,26 @@ void vecAdd(float* A, float* B, float* C, int n) {
 }
 
 int main() {
+    int N = 1 << 20; // 1 million elements
+    size_t size = N * sizeof(float);
+
+    // Host vectors
+    float* h_A = (float*)malloc(size);
+    float* h_B = (float*)malloc(size);
+    float* h_C = (float*)malloc(size);
+
+    for (int i = 0; i < N; ++i) {
+        h_A[i] = i;
+        h_B[i] = i * 2;
+    }
+
+    vecAdd(h_A, h_B, h_C, N);
+
+    std::cout << "C[0] = " << h_C[0] << ", C[N-1] = " << h_C[N-1] << std::endl;
+
+
+    free(h_A); free(h_B); free(h_C);
+
+    return 0;
 
 }
