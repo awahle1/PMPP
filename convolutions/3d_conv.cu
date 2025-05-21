621aaeb3d0f3e415d311845a193b0837b1f4f563
Austin Wahle <wahle@umich.edu>
2025-05-21 08:26:36 -0700

    Clean up 3d kernel

--- convolutions/3d_conv.cu
+++ convolutions/3d_conv.cu
@@ -3,26 +3,26 @@
 #include <iomanip>
 
 __global__
-void 3dConvKernel(float *M, float*F, float*P, int M_SIZE, int F_WIDTH){
-    int plane = threadIdx.z + blockDim.z*blockIdx.z;
-    int row = threadIdx.y + blockDim.y*blockIdx.y;
-    int col = threadIdx.x + blockDim.x*blockIdx.x;
+void 3dConvKernel(float *M, float*F, float*P, int width, int height, int depth, int r){
+    int outPlane = threadIdx.z + blockDim.z*blockIdx.z;
+    int outRow = threadIdx.y + blockDim.y*blockIdx.y;
+    int outCol = threadIdx.x + blockDim.x*blockIdx.x;
 
-    int i_l = plane*M_SIZE*M_SIZE + row*M_SIZE + col;
+    int F_WIDTH = 2*r + 1
 
-    if (i_m < M_SIZE**3){
-        return;
-    }
     PValue = 0;
     for (int fplane=0; fplane<=F_WIDTH; ++fplane){
         for (int frow=0; frow<=F_WIDTH; ++frow){
             for (inf fcol=0; fcol<=F_WIDTH; ++fcol){
-                int i_m = i_l - (M_SIZE**2(fplane-2)+ M_SIZE*(frow-2) + (fcol-2))
-                if (i_m < 0 || i_m > M_SIZE**3){
+                int inCol = outCol + (fcol-2);
+                int inRow = outRow + (frow-2);
+                int inPlane = outPlane + (fplane-2)
+                if (inCol < 0 || inRow<0 || inPlane<0 || inCol > width || inRow > height || inPlane>depth){
                     continue;
                 }
                 
-                int i_f = fplane * F_WIDTH**2 + frow*F_WIDTH + fcol
+                int i_f = fplane * F_WIDTH**2 + frow*F_WIDTH + fcol;
+                int i_m = inPlane*width*height + inRow*width + inCol;
                 PValue += F[i_f] * M[i_m];
                 
             }
